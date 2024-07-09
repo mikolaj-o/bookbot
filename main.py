3edd1fd447441d611e799f13dc4ac93de3faabbd
mikolaj-o <olczynskie@gmail.com>
2024-07-09 07:14:23 +0200

    B: Kolejne zadanie, REPORT do skonczenia

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 
 def main(low_case_text):
     character_dict = {}
+    i = 0
     for character in low_case_text:
-        character_dict[character] = 1
+        
 
-        return character_dict
+        if character in character_dict and character.isalpha():
+            character_dict[character] += 1 
+        elif character.isalpha():
+            character_dict[character] = 1 
+        
+    return character_dict
         
 
 def contents():
@@ -21,11 +27,20 @@ def lower_case_contents(file_contents):
         low_case_text = file_contents.lower()
         return low_case_text
 
+# kod pomiedzy komentarzami do skonczenia
+
+def make_dict_list(dict):
+      book_dict_list = []
+      for record in dict:
+            
+# kod pomiedzy komentarzami do skonczenia            
+      
+
 z = contents()
 
 y = lower_case_contents(z)
 
-x = main(y)
+char_dict = main(y)
 
-print(y)
-print(x)
+# print(y)
+print(char_dict)
